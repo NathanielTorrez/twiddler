0cf153b8f162f4691f8b8986b20ba4d10c6a65e4
Nathaniel Torrez <nathanieltorrez@icloud.com>
2020-03-05 14:52:32 -0800

    Fully functioning
    Created a function that is currently running automatically instead of with a button to be more accurate. Also created a return home button for when you click someones name. The colors used are from twitter directly in an effort to make it more realistic.

--- operations.js
+++ operations.js
@@ -1,34 +1,48 @@
- $(document).ready(function(){
-    
-        var $bank = $('#tweetbank');
-        var $new  = $("#newTweets");
+  $(document).ready(function(){
+        
+        setInterval(autoRefresh,3000)
+        
+      
         var $body = $('body');
-
-
-
-        $body.html();                
+        $body.html();
         var index = streams.home.length - 1;
         while(index >= 0){
           var tweet = streams.home[index];
           var $tweet = $('<div></div>');
-          $tweet.html("<div id='user'><p>@" + tweet.user + ": " + tweet.message +"</div><div id='timestamp'>( " + tweet.created_at +" )</p></div>");
-          $tweet.appendTo($bank);
+          $tweet.html( "<div class='tweet_user'><br><p>"+ tweet.user + "</div>" + "<div class='tweet'>" + tweet.message + "</p><p>" + "<div class='time'>" + tweet.created_at + "</p><br>");
+          $tweet.appendTo('#twiddlerhome');
           index -= 1;
         }
 
-        $(".update").click(function(){
+        function autoRefresh(){
+          var $home=$('#twiddlerhome');
+          $home.html(''); 
+          
+          var i = streams.home.length - 1;
+          while(i >= 0){
+            var tweet = streams.home[i];
+            var $tweet = $('<div></div>');
+            $tweet.html("<div class='tweet_user'><br><p>"+ tweet.user + "</div>" + "<div class='tweet'>" + tweet.message + "</p><p>" + "<div class='time'>" + tweet.created_at + "</p><br>");
+            $tweet.appendTo('#twiddlerhome');
+            i -= 1;
+            
+        }
+        $(".tweet_user").click(function(){
+          var $home=$('#twiddlerhome');
+          var thisName = $(this).text(); 
+          var userNameArr =streams.users[thisName];
 
-          $bank.html('');
-          var index = streams.home.length - 1;
+          for(var i = 0; i < userNameArr.length; i++) {
+              console.log("the tweets",userNameArr[i].message);
+              
+              var $tweet = $('<div></div>');
+              $tweet.html("<div class='tweet_user'><br><p>" + thisName + "</div>" + "<div class='tweet'>" + userNameArr[i].message + "</p><p>" + "<div class='time'>" + tweet.created_at + "</p><br>");
+              $home.slideUp();
+              $('#twiddlerUserPage').slideDown(); 
+              $tweet.prependTo($('#twiddlerUserPage')).hide().slideDown("slow");
+              }
+        });  
+      }
+      setInterval(autoRefresh,3000)
 
-
-         while(index >= 0){
-          var tweet = streams.home[index];
-          var $tweet = $('<div></div>');
-          $tweet.html("<div id='user'><p>@" + tweet.user + ": " + tweet.message +"</div><div id='timestamp'>( " + tweet.created_at +" )</p></div>");
-          $tweet.appendTo($bank);
-          index -= 1;
-        }});
-
-        
-      });
+    });
